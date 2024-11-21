3a4b6492e3a7e6ed3e11a7caa07b99729cd7b278
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-11-21 17:43:05 +0000

    database connected

--- dispenser/src/main/java/io/github/somunaexe/dispenser/run/RunRepository.java
+++ dispenser/src/main/java/io/github/somunaexe/dispenser/run/RunRepository.java
@@ -13,40 +13,40 @@ import java.util.Optional;
 public class RunRepository {
     private List<Run> runs = new ArrayList<>();
 
-    List<Run> findAll() {
-        return runs;
-    }
-
-    Optional<Run> findById(Integer id) {
-        return runs.stream().filter(run -> Objects.equals(run.id(), id)).findFirst();
-    }
-
-    void create(Run run) {
-        runs.add(run);
-    }
-
-    void update(Run run, Integer id) {
-        Optional<Run> existingRun = findById(id);
-        existingRun.ifPresent(value -> runs.set(runs.indexOf(value), run));
-    }
-
-    void delete(Integer id) {
-        runs.removeIf(run -> run.id().equals(id));
-    }
-    @PostConstruct
-    private void init() {
-        runs.add(new Run(1,
-            "Monday Run",
-            LocalDateTime.now(),
-            LocalDateTime.now().plusMinutes(30),
-            3,
-            Location.INDOOR));
-
-        runs.add(new Run(2,
-            "Wednesday Run",
-            LocalDateTime.now(),
-            LocalDateTime.now().plusMinutes(60),
-            6,
-            Location.OUTDOOR));
-    }
+//    List<Run> findAll() {
+//        return runs;
+//    }
+//
+//    Optional<Run> findById(Integer id) {
+//        return runs.stream().filter(run -> Objects.equals(run.id(), id)).findFirst();
+//    }
+//
+//    void create(Run run) {
+//        runs.add(run);
+//    }
+//
+//    void update(Run run, Integer id) {
+//        Optional<Run> existingRun = findById(id);
+//        existingRun.ifPresent(value -> runs.set(runs.indexOf(value), run));
+//    }
+//
+//    void delete(Integer id) {
+//        runs.removeIf(run -> run.id().equals(id));
+//    }
+//    @PostConstruct
+//    private void init() {
+//        runs.add(new Run(1,
+//            "Monday Run",
+//            LocalDateTime.now(),
+//            LocalDateTime.now().plusMinutes(30),
+//            3,
+//            Location.INDOOR));
+//
+//        runs.add(new Run(2,
+//            "Wednesday Run",
+//            LocalDateTime.now(),
+//            LocalDateTime.now().plusMinutes(60),
+//            6,
+//            Location.OUTDOOR));
+//    }
 }
